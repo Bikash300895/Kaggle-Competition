b6274666ae63d25c9db0eeed79ad3b7a06d54331
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-22 19:21:15 +0600

    jupyter notbook file initinlized
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

--- Titanic/code/Predicting Survival.py
+++ Titanic/code/Predicting Survival.py
@@ -31,25 +31,5 @@ pylab.rcParams[ 'figure.figsize' ] = 8 , 6
 
 
 
-"""Helpepr functions"""
-def plot_correlation_map(df, cat, target, **kwargs):
-    pass
-
-
-
 # Loading data
-train = pd.read_csv("../input/train.csv")
-test = pd.read_csv("../input/test.csv")
-
-full = train.append(test, ignore_index = True)
-titanic=full[:891]
-del train, test
-
-
-
-"""Statistical symmaries and viasualisations"""
-# Show first few rows of data
-titanic.head()
-
-# show statistical summary
-titanic.describe()
+data = pd.read_csv("../input/train.csv")
