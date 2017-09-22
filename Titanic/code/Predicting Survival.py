1cedc8be0e81551cabae012934ed91a1a3f01a16
Shuvendu Bikash <bikash11roy@gmail.com>
2017-09-22 12:15:03 +0600

    adding data
    Signed-off-by: Shuvendu Bikash <bikash11roy@gmail.com>

---
+++ Titanic/code/Predicting Survival.py
@@ -0,0 +1,34 @@
+import warnings
+warnings.filterwarnings('ignore')
+
+# Handling table like data and matrices
+import numpy as np
+import pandas as pd
+
+# Modeling algorithm
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC, LinearSVC
+from sklearn.ensemble import RandomForestClassifier , GradientBoostingClassifier
+
+# Modelling Helpers
+from sklearn.preprocessing import Imputer , Normalizer , scale
+from sklearn.model_selection import train_test_split , StratifiedKFold
+from sklearn.feature_selection import RFECV
+
+# Visualisation
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.pylab as pylab
+import seaborn as sns
+
+mpl.style.use( 'ggplot' )
+sns.set_style( 'white' )
+pylab.rcParams[ 'figure.figsize' ] = 8 , 6
+
+
+
+# Loading data
+train = pd.read_clipboard
